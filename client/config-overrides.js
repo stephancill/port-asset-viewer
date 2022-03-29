536557a30e15e81c7c321777f0df4926a98d4cc0
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-03-29 17:35:54 +0200

    Babel workaround

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,4 +1,4 @@
-const { removeModuleScopePlugin, override, babelInclude, getBabelLoader } = require("customize-cra");
+const { removeModuleScopePlugin, override, babelInclude, useBabelRc, addExternalBabelPlugin } = require("customize-cra");
 const path = require("path");
 
 module.exports = override(
@@ -8,4 +8,10 @@ module.exports = override(
     path.resolve("../backend/types")
     // path.resolve("../lib"),  // (2)
   ]),
+
+  // This shit is because of the IPFS module
+  // https://github.com/paulmillr/noble-ed25519/issues/34
+  addExternalBabelPlugin("@babel/plugin-proposal-nullish-coalescing-operator"),
+  addExternalBabelPlugin("@babel/plugin-proposal-optional-chaining"),
+  addExternalBabelPlugin("@babel/plugin-syntax-bigint")
 );
